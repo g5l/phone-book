2fd6dc74aeddc44a218165bfbb4630b1821194b0
Gabriel Debona <gabriel.debona@gmail.com>
2023-06-17 21:42:54 -0300

    fix: Fixed tailwindcss wasn't working

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Main from './pages/Main.tsx'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
 )
