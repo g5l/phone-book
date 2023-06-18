9daddefad020da7dfcd8d60904f330f5c6969f08
Gabriel Debona <gabriel.debona@gmail.com>
2023-06-17 23:13:18 -0300

    feat: Created ContactItem to display the contact data

---
+++ src/components/ContactItem/ContactItem.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Contact} from "../../types";
+
+interface Props {
+  contact: Contact
+}
+
+const ContactItem: React.FC<Props> = ({contact}) => {
+  return (
+    <div className="flex justify-between items-center bg-white p-5 my-5 rounded-md">
+      <div>
+        <p className="text-xl">{contact.name}</p>
+        <p className="text-sm text-gray-400">{contact.phone}</p>
+      </div>
+      <button className="py-3 px-10 rounded-md bg-red-700 hover:bg-red-600 text-white">
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default ContactItem;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,26 @@
-import './App.css'
+import './Main.css'
 import Header from "../components/Header/Header.tsx";
+import {useState} from "react";
+import ContactItem from "../components/ContactItem/ContactItem.tsx";
+import {Contact} from "../types";
 
 function Main() {
-  // const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState<Contact[]>([{
+    id: 1,
+    name: 'John Doe',
+    phone: '555-555-5555',
+  }, {
+    id: 2,
+    name: 'Gabriel Debona',
+    phone: '555-555-5555',
+  }])
 
   return (
     <div className="container mx-auto">
       <div className="max-w-2xl bg-gray-200 rounded-md p-8 mx-auto mt-10">
         <h1 className="text-center text-2xl mb-10">Phone Book App</h1>
         <Header/>
+        {contacts.map(contact => <ContactItem key={contact.id} contact={contact}/>)}
       </div>
     </div>
   )
