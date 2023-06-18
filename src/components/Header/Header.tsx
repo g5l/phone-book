4aa024d16e011d540182bb9c9c3ebf41c834e7b6
Gabriel Debona <gabriel.debona@gmail.com>
2023-06-17 21:45:28 -0300

    feat: Created Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="flex justify-between content-center">
+      <h2 className="text-xl">Contacts</h2>
+      <button className="py-3 px-10 rounded-md bg-sky-700 text-white">Add Contact</button>
+    </div>
+  );
+}
+
+export default Header;
